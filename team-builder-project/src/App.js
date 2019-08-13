690d16b559d309b9d7a832519d69b9198baf435b
JamesB <jblazer1@yahoo.com>
2019-08-12 20:59:29 -0700

    Set up file structure

--- team-builder-project/src/App.js
+++ team-builder-project/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import foobar from "./data";
 
 function App() {
+  const [team, setTeam] = useState(foobar);
+
+  const addNewMember = member => {
+    setTeam([...team, team]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">TEAM MEMBERS</header>
+      <TeamForm addNewMember={addNewMember} />
+      <Member memberList={team} />
     </div>
   );
 }
